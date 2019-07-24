34bd71aaaf638d85e0a79a5a8efc75993d8c9c84
HadasKedem <41523905+HadasKedem@users.noreply.github.com>
2019-07-24 14:34:29 +0300

    Delete module2.js

--- module2.js
+++
@@ -1,7 +0,0 @@
-const module2 = angular.module("module2", [])
-
-module2.controller("HelloCtrl2", HelloCtrl2);
-
-function HelloCtrl2() {
-    this.message = "Hello I am controller in second module"
-}
