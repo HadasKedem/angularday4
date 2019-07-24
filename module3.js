9374ce34a589644e79f54854c3a8af9465af2a38
HadasKedem <41523905+HadasKedem@users.noreply.github.com>
2019-07-24 14:35:02 +0300

    Delete module3.js

--- module3.js
+++
@@ -1,7 +0,0 @@
-const module3 = angular.module("module3", [])
-
-module3.controller("HelloCtrl3", HelloCtrl3);
-
-function HelloCtrl3() {
-    this.message = "Hello I am controller in third module"
-}
